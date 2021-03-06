978534c727dc5ef09f59b1434daff3960a622ae0
Francis Rodrigues <francis.rod01@outlook.com>
2021-03-05 23:33:15 -0300

    wip(functions): send message with plivo client

---
+++ functions/index.js
@@ -0,0 +1,13 @@
+const functions = require('firebase-functions');
+const plivo = require('plivo');
+
+const client = new plivo.Client('YOUR_ID', 'YOUR_TOKEN');
+
+exports.sendInvite = functions.firestore.document('invites/{phoneNumber}').onCreate(async (doc) => {
+  const from = '+YOUR_PLIVO_NUMBER';
+  const to = doc.data().phoneNumber;
+
+  const text = 'You are one of the collest kids now!';
+
+  return client.messages.create(from, to, text);
+});
